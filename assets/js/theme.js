52e9061205a17c7a9353ce22aae63af56bd061a1
Bryce Kwon <me@brycekwon.com>
2025-02-14 01:01:56 -0800

    refactor: prune unused javascript code

--- assets/js/theme.js
+++ assets/js/theme.js
@@ -20,7 +20,7 @@ function themeToggle() {
             document.body.classList.add('dark');
             localStorage.setItem("pref-theme", 'dark');
         }
-    })
+    });
 }
 
 function showCodeCopyButtons() {
@@ -44,31 +44,10 @@ function showCodeCopyButtons() {
                 copyingDone();
                 return;
             }
-
-            const range = document.createRange();
-            range.selectNodeContents(codeblock);
-            const selection = window.getSelection();
-            selection.removeAllRanges();
-            selection.addRange(range);
-            try {
-                document.execCommand('copy');
-                copyingDone();
-            } catch {
-                console.error("Could not copy")
-            };
-            selection.removeRange(range);
         });
 
         if (container.classList.contains("highlight")) {
             container.appendChild(copybutton);
-        } else if (container.parentNode.firstChild == container) {
-            // td containing LineNos
-        } else if (codeblock.parentNode.parentNode.parentNode.parentNode.parentNode.nodeName == "TABLE") {
-            // table containing LineNos and code
-            codeblock.parentNode.parentNode.parentNode.parentNode.parentNode.appendChild(copybutton);
-        } else {
-            // code blocks not having highlight as parent class
-            codeblock.parentNode.appendChild(copybutton);
         }
     });
 }
