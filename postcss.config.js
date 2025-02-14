904f85d77c9479a6fa495534b39a6a31eaff29c0
Bryce Kwon <me@brycekwon.com>
2025-02-14 00:53:07 -0800

    fix: stop purgecss from pruning code highlighting

--- postcss.config.js
+++ postcss.config.js
@@ -8,10 +8,18 @@ const purgecss = purgeCSSPlugin({
         const els = JSON.parse(content).htmlElements;
         return [...(els.tags || []), ...(els.classes || []), ...(els.ids || [])];
     },
-    safelist: [],
+    safelist: [
+        'copy-code', 'code', 'chroma', 'x', 'err', 'cl', 'lnlinks', 'lntd', 'lntable', 'hl',
+        'lnt', 'ln', 'line', 'k', 'kc', 'kd', 'kn', 'kp', 'kr', 'kt', 'n', 'na', 'nb', 'bp', 'nc',
+        'no', 'nd', 'ni', 'ne', 'nf', 'fm', 'nl', 'nn', 'nx', 'py', 'nt', 'nv', 'vc', 'vg', 'vi',
+        'vm', 'l', 'ld', 's', 'sa', 'sb', 'sc', 'dl', 'sd', 's2', 'se', 'sh', 'si', 'sx', 'sr',
+        's1', 'ss', 'm', 'mb', 'mf', 'mh', 'mi', 'il', 'mo', 'o', 'ow', 'p', 'c', 'ch', 'cm',
+        'c1', 'cs', 'cp', 'cpf', 'g', 'gd', 'ge', 'gr',  'gh', 'gi', 'go', 'gp', 'gs', 'gu', 'gt',
+        'gl', 'w',
+    ],
 });
 
-/** @type {import("postcss").Config} */
+/** @type {import('postcss').Config} */
 export default {
     plugins: [
         autoprefixer,
